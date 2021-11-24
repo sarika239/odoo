2510d42825f39cfa3c841eaaa1f1bae50c8c1ee1
sarika <82756929+sarika239@users.noreply.github.com>
2021-11-24 07:45:40 +0530

    Add files via upload

---
+++ library/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Library Odoo',
+    'category': 'Sales',
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/books_menu.xml',
+    ],
+}
